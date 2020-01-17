bb32fe30192d2972827eab54250acfc0cdb45975
manish nagar <33551293+manishnagar1995@users.noreply.github.com>
2020-01-17 12:54:02 +0530

    update 17 jan

--- AddProducts/force-app/main/default/lwc/addProductLWC/addProductLWC.js
+++ AddProducts/force-app/main/default/lwc/addProductLWC/addProductLWC.js
@@ -1,34 +1,80 @@
-import { LightningElement, track  } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement, track,wire,api } from 'lwc';
 import serachProds from '@salesforce/apex/SearchController.retriveProducts';
-import showallprodlist from '@salesforce/apex/SearchController.retriveallProducts';
-
-const columns = [
-    {
-        label: 'Name',
-        fieldName: 'Name',
-        type: 'url',
-        typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}
-    }, {
-        label: 'Product Code',
-        fieldName: 'ProductCode',
-        type: 'text',
-    }, 
-    ];
+import showallprod from '@salesforce/apex/SearchController.retriveallProducts';
+//mport showallprodlist from '@salesforce/apex/SearchController.retriveallProducts';
+import { refreshApex } from '@salesforce/apex';
 
 export default class addProductLWC extends LightningElement {
     @track openmodel = false;
-    @track searchData;
-    @track columns = columns;
-    @track strSearchProdName;
+    @track areDetailsVisible = false;
+    @track searchData;//@track data;
+    @track error;
+    @api sortedDirection = 'asc';
+    @api sortedBy = 'Name';
+    @api searchKey = '';
+    @api recordId;
+   result;showdprod;
+    
+    
+    @track columns = [
+        {
+            label: 'Name',
+            fieldName: 'Name',
+            type: 'url',
+            sortable: true,
+            typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}
+        }, {
+            label: 'Product Code',
+            fieldName: 'ProductCode',
+            type: 'text',
+            sortable: true
+        }, 
+        ];
+
+    @track strSearchProdName ;
      
-    connectedCallback() {
-        this.searchData = showallprodlist;
+    @wire(showallprod, {oppId:'$recordId' })
+    getprod(showdprod) {
+        this.showdprod = showdprod;
+        if (showdprod.data) {
+            this.areDetailsVisible = true;
+            this.searchData = showdprod.data;
+           
+            console.log('datashow================', this.searchData );
+        } else if (showdprod.error) {
+            this.error = showdprod.error;
+        }
+    }
+    @wire(serachProds, {searchKey: '$searchKey', sortBy: '$sortedBy', sortDirection: '$sortedDirection' })
+    getProductssearch(result) {
+        this.result = result;
+        if (result.data) {
+            this.searchData = result.data;
+            console.log('datashow================', this.searchData );
+        } else if (result.error) {
+            this.error = result.error;
+        }
+    }
+    sortColumns( event ) {
+        this.sortedBy = event.detail.fieldName;
+        this.sortedDirection = event.detail.sortDirection;
+        return refreshApex(this.result);
+       
+    }
+    handleProductName( event ) {
+        this.searchKey = event.target.value;
+        return refreshApex(this.result);
     }
 
-
-
     openmodal() {
-        this.openmodel = true
+        this.openmodel = true;
+        // eslint-disable-next-line no-console
+        console.log('openmodel===',this.openmodel)
+       // this.template.querySelector('.gr__manishnagar-dev-ed_lightning_force_com').style.overflow = "hidden"; 
+        // eslint-disable-next-line no-console
+       // console.log('desktopclass',this.template.querySelector('.desktop'))
+       // document.body.setAttribute('style', 'overflow: visible;');
     }
     closeModal() {
         this.openmodel = false
@@ -38,12 +84,6 @@ export default class addProductLWC extends LightningElement {
         alert('save method invoked');
         this.closeModal();
     }
-
-    handleProductName(event) {
-        this.strSearchProdName = event.detail.value;
-        serachProds({strProdName : this.strSearchProdName})
-        .then(result => {
-        this.searchData = result;
-    })
-}
+  
+  
 }
